a2e030be7396d564853c431935af7c9c593e25f5
maityabhishek <a.maity@outlook.com>
2021-04-15 21:53:46 +0530

    Added the speech to text code

---
+++ from-microphone/src/speechsdk/quickstart/Program.java
@@ -0,0 +1,21 @@
+package speechsdk.quickstart;
+import com.microsoft.cognitiveservices.speech.*;
+import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class Program {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        SpeechConfig speechConfig = SpeechConfig.fromSubscription("49a9dfb1f4ae410ebbb4ee7e31bb103f", "eastus");
+        fromFile(speechConfig);
+    }
+
+    public static void fromFile(SpeechConfig speechConfig) throws InterruptedException, ExecutionException {
+        AudioConfig audioConfig = AudioConfig.fromWavFileInput("/Users/abhishekmaity/Desktop/MIMapp/cognitive-services-speech-sdk/quickstart/java/jre/from-microphone/src/speechsdk/quickstart/ad.wav");
+        SpeechRecognizer recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+        
+        Future<SpeechRecognitionResult> task = recognizer.recognizeOnceAsync();
+        SpeechRecognitionResult result = task.get();
+        System.out.println("RECOGNIZED: Text=" + result.getText());
+    }
+}
